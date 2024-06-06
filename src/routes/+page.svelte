94ed8161056307bd9f16df32a3b4bcc1047511e8
Mr. Hullen <hullenda@craighead.school.nz>
2024-06-06 01:34:04 +0000

    Adds an example of using stores.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  import { name } from '$lib/stores'
 </script>
 
-<Header />
+<a href="/update-name">Update Name</a>
 
-<main>
-  <h2>SvelteKit</h2>
-
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
-</main>
-
-<footer>
-  <p>&copy; Craighead Diocesan School 2024</p>
-</footer>
+{#if name}
+  <p>The name is {$name}</p>
+{:else}
+  <p>No name yet.</p>
+{/if}
